b5743ad41df5787da0e9efecbdca95f8c0858081
Peter Phan <pphan@hashicorp.com>
2019-09-27 14:21:12 -0700

    change type

--- main.tf
+++ main.tf
@@ -12,40 +12,40 @@ variable "gcp_project" {
 
 variable "gcp_region" {
   description = "GCP region, e.g. us-east1"
-  default = "us-west1"
+  default     = "us-west1"
 }
 
 variable "gcp_zone" {
   description = "GCP zone, e.g. us-east1-a"
-  default = "us-west1-b"
+  default     = "us-west1-b"
 }
 
 variable "machine_type" {
   description = "GCP machine type"
-  default = "g1-small" # Must be n1-standard-1 at start of demo
+  default     = "g1-small" # Must be n1-standard-1 or g1-small at start of demo
 }
 
 variable "instance_name" {
   description = "GCP instance name"
-  default = "demo"
+  default     = "demo"
 }
 
 variable "image" {
   description = "image to build instance from"
-  default = "debian-cloud/debian-9"
+  default     = "debian-cloud/debian-9"
 }
 
 provider "google" {
   # credentials = "${var.gcp_credentials}"
-  project     = "${var.gcp_project}"
-  region      = "${var.gcp_region}"
+  project = "${var.gcp_project}"
+  region  = "${var.gcp_region}"
 }
 
 resource "google_compute_instance" "demo" {
-  name         = "${var.instance_name}"
-  machine_type = "${var.machine_type}"
-  zone         = "${var.gcp_zone}"
-  allow_stopping_for_update= true
+  name                      = "${var.instance_name}"
+  machine_type              = "${var.machine_type}"
+  zone                      = "${var.gcp_zone}"
+  allow_stopping_for_update = true
   boot_disk {
     initialize_params {
       image = "${var.image}"
@@ -62,6 +62,6 @@ resource "google_compute_instance" "demo" {
 
 }
 
-output "external_ip"{
+output "external_ip" {
   value = "${google_compute_instance.demo.network_interface.0.access_config.0.assigned_nat_ip}"
 }
